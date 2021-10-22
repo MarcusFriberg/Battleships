1a29e2afc85e8bf84fa5b2aceee44eca719f3fa0
MarcusFriberg <76865631+MarcusFriberg@users.noreply.github.com>
2021-10-22 13:12:11 +0200

    Initial commit

---
+++ src/com/edugames/Main.java
@@ -0,0 +1,17 @@
+package com.edugames;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+}
