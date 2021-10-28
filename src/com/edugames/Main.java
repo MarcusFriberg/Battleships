a812072388de521f1297aaa7f75a746bf711ea71
MarcusFriberg <76865631+MarcusFriberg@users.noreply.github.com>
2021-10-28 19:49:21 +0200

    added launch(args)

--- src/com/edugames/Main.java
+++ src/com/edugames/Main.java
@@ -6,16 +6,11 @@ import javafx.stage.Stage;
 public class Main extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+	launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Battleships");
-            //Create and show a new scene "StartView" in primaryStage
-            //Updates the scene to "GameView" and starts a new session when the buttons are pressed
-
+    // Insert code here
     }
-
 }
-
