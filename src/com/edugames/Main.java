a352bdd7d951135b40c0a154bc99ca246dd67760
MarcusFriberg <76865631+MarcusFriberg@users.noreply.github.com>
2021-10-31 12:25:18 +0100

    Added buttonimages to resource folder, Main initiates a startView and presents it. Startview created with a method to present itself.

--- src/com/edugames/Main.java
+++ src/com/edugames/Main.java
@@ -1,6 +1,13 @@
 package com.edugames;
 
+import com.edugames.view.StartView;
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -10,7 +17,12 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-    // Insert code here
+    public void start(Stage primaryStage) {
+        // Set the title of the window and create a StackPane
+        primaryStage.setTitle("Sänka skepp");
+
+        // Create a new startView and provide it with the primaryStage and let it present itself
+        StartView startView = new StartView(primaryStage);
+        startView.present();
     }
 }
--- src/com/edugames/view/StartView.java
+++ src/com/edugames/view/StartView.java
@@ -1,9 +1,73 @@
 package com.edugames.view;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
 
 public class StartView {
+    // Variables
+    private Stage primaryStage;
 
-    //Insert code launch screen with two buttons "Start client" and "Start server"
-    //Buttons makes call to GameSession with the parameter of client or server
-    //Returns a StartView scene
-    //
+    // Constructor
+    public StartView(Stage primaryStageStage) {
+        this.primaryStage = primaryStageStage;
+    }
+
+    public void present() {
+        // Create two buttons to let user select game mode
+        Button buttonStartServer = new Button("", new ImageView("serverstart.png"));
+        buttonStartServer.setStyle("-fx-background-color:transparent");
+        buttonStartServer.setScaleX(0.5);
+        buttonStartServer.setScaleY(0.5);
+        buttonStartServer.setOnMouseEntered(event -> {
+            buttonStartServer.setScaleX(0.55);
+            buttonStartServer.setScaleY(0.55);
+        });
+        buttonStartServer.setOnMouseExited(event -> {
+            buttonStartServer.setScaleX(0.5);
+            buttonStartServer.setScaleY(0.5);
+        });
+        buttonStartServer.setOnAction(event -> {
+            System.out.println("Server mode was pressed");
+        });
+        Button buttonStartClient = new Button("", new ImageView("clientstart.png"));
+        buttonStartClient.setStyle("-fx-background-color:transparent");
+        buttonStartClient.setScaleX(0.5);
+        buttonStartClient.setScaleY(0.5);
+        buttonStartClient.setOnMouseEntered(event -> {
+            buttonStartClient.setScaleX(0.55);
+            buttonStartClient.setScaleY(0.55);
+        });
+        buttonStartClient.setOnMouseExited(event -> {
+            buttonStartClient.setScaleX(0.5);
+            buttonStartClient.setScaleY(0.5);
+        });
+        buttonStartClient.setOnAction(event -> {
+            System.out.println("Client mode was pressed");
+        });
+        // Create a VBox to hold the buttons and place it under the game-logo
+        VBox buttonsBox = new VBox();
+        buttonsBox.setPadding(new Insets(240, 10, 10, 10));
+        buttonsBox.setSpacing(-50);
+        buttonsBox.getChildren().addAll(buttonStartServer, buttonStartClient);
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(buttonsBox);
+        // Create a new image called startScreenImage from Start-screen.png
+        Image startScreenImage = new Image("Start-screen.png");
+        // Create a new BackgroundImage from the startScreenImage and make it none repeat, position to CENTER and make it default size
+        BackgroundImage backgroundImage = new BackgroundImage(startScreenImage, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
+        // Create a Background from the backgroundImage
+        Background background = new Background(backgroundImage);
+        // Set the background of stackPane to this background
+        stackPane.setBackground(background);
+        // Make a new Scene containing the stackPane and set the size as the backgrounds size
+        Scene scene = new Scene(stackPane, 960, 768);
+        // Set this scene as the scene of primaryStage
+        primaryStage.setScene(scene);
+        // Show the stage
+        primaryStage.show();
+    }
 }
