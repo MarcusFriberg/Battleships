0153dbc2835c2b6bfb25c89d7b3d562c15ab0231
matildawintence <matilda@wintence.com>
2021-11-08 20:04:45 +0100

    Continued working on GameSession.java and made comments and methods

--- src/com/edugames/model/GameSession.java
+++ src/com/edugames/model/GameSession.java
@@ -1,69 +1,93 @@
 package com.edugames.model;
-
+// Imports
 import com.edugames.controller.GameController;
-import com.sun.org.apache.xpath.internal.operations.Bool;
-
+/*
+ * Class GameSession
+ * A class that communicates via sockets with another computer, decodes/encodes the in/out parameters and communicate
+ * with GameController.
+ * @param: Constructor receives the isServer(Boolean) and gameController(GameController) as parameter.
+ * @author: Matilda Wintence
+ * @author: matilda.wintence.edu.edugrade.com
+ * @co-author: Martin Andersson
+ * @co-author: martin.andersson.edu.edugrade.com
+ * @version: 1.0
+ */
 public class GameSession {
-
-    Boolean isServer;
-    private String si;
-    private String s1;
-    private String s2;
-    private String s3;
-    private String s4;
-    private String incoming;
+    // Variables
+    private Boolean isServer;
+    private Coordinate lastEnemyCoordinateShot;
     private GameController gameController;
-    private int shotFired;
-    // Detta tillhör Martins:
-    // private int totalShotsFired;
-    private String lastShotFired;
-
-
-    //Make a constructor that takes parameters to start a new session as client or server
-    //Create a new server connection
-    //Create a GameController and send a parameter with shoot or wait
-    //Create methods to send information about incoming shots to the GameController
-    //Create methods to receive information from GameController about shots fired and results of enemy shots
-    //
-
-/*    Vi behöver kunna skicka information till GameController
-- om alla inkommande skott
-- om det föregående skottet blev en träff (kräver att vi “kommer ihåg” koordinaterna för vårt senaste skott)
-    Vi behöver kunna ta emot information från GameController
-- om vilken koordinat vi beskjuter på motståndarens spelplan
-- om motståndarens senaste skott blev en träff*/
-
-    // TODO: Är tanken att jag ska göra en metod för att ta emot och skicka för varje s1,s2,s3,s4 osv eller bara en
-    // TODO: metod som tar emot och tolkar och sparar det i en incoming String och sen skickar det till GameController?
-
-    public GameSession(Boolean isServer) {
+    // Constructor
+    public GameSession(Boolean isServer, GameController gameController) {
         this.isServer = isServer;
-    }
-
-    public void initGameSession(String si) {
-        this.si = si;
-        lastShotFired = si;
-        // TODO: Send to socket here or in a separate method?
+        this.gameController = gameController;
+        if(isServer) {
+            //server
+        } else {
+            //client
         }
+    }
+    /*
+     * Method hostServer
+     * A method connect with another computer as server, on the same network.
+     * Creates a loop in which in och out parameters can pass trough.
+     * @author: Martin Andersson
+     * @author: martin.andersson.edu.edugrade.com
+     * @version: 1.0
+     */
+    // TODO: Two methods for client and server - ropa på metoder - tex en som bakar ihop strängen som ska skickas
+    /*
+     * Method hostClient
+     * A method connect with another computer as client, on the same network.
+     * Creates a loop in which in och out parameters can pass trough.
+     * @author: Martin Andersson
+     * @author: martin.andersson.edu.edugrade.com
+     * @version: 1.0
+     */
 
 
-    public String incomingFire() {
-        // TODO: Divide into sub strings and only send the coordinates?? Sockets incoming here?
-        return s1;
+    /*
+     * Method decodeIncomingData
+     * A method that has a String as an inparameter.
+     * Decodes the incoming data and sends it to the bridge.
+     * @author: Matilda Wintence
+     * @author: matilda.wintenceg@edu.edugrade.se
+     * @version: 1.0
+     */
+    public void decodeIncomingData(String incomingData) {
+        String[] incomingDataSplit = incomingData.split(" ");
+        //send incomingDataSplit[0] and incomingDataSplit[2]
     }
 
-    public void outgoingFire(String s1) {
-        // TODO: Send to socket
+        /*
+     * Method encodeOutgoingData
+     * A method that has a String and a Coordinate object as an inparameter.
+     * Encodes the outgoing data and sends it to the bridge.
+     * @author: Matilda Wintence
+     * @author: matilda.wintenceg@edu.edugrade.se
+     * @version: 1.0
+     */
+    public void encodeOutGoingData(String outgoingData, Coordinate coordinateFiredOn) {
+        String coordinateFiredOnString = coordinateFiredOn.toString();
+        String encodedOutgoingData = outgoingData.concat(coordinateFiredOnString);
     }
 
 
-    //TODO: Är detta rätt sätt:
+    /*
+     * Method bridgeIncomingAndOutgoingData
+     * A method that acts as a bridge between the hostClient/hostServer and decodeIncomingData/encodeOutgoingData.
+     * Sends the data between different mothods.
+     * @author: Matilda Wintence
+     * @author: matilda.wintenceg@edu.edugrade.se
+     * @version: 1.0
+     */
 
+        // sista koordinaten vi sköt på hos motståndaren - lagra i en variabel som heter lastEnemyCoordinateShot
 
-    public void incomingData(String incoming) {
-        //TODO: Dela upp i substrängar eller index samt en if (subString1 == "h")/ if (incoming.plats(0) == "h"
-        // så skickas det till GameController att det har blivit en träff. På så vis kan alla ligga under incoming
+        //handleIncomingFire (en ev framtida metod i gameController)
+
+        //bridge lägg till en sleep/wait (1000)
     }
 
 
-}
+
