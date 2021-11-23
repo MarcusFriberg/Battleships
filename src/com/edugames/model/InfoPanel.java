9cdad54adbbbfc49a686947dab9bf9172ec08ee3
MarcusFriberg <76865631+MarcusFriberg@users.noreply.github.com>
2021-11-23 23:05:21 +0100

    Removed content in infoPanel and its dependencies.

--- src/com/edugames/model/InfoPanel.java
+++ src/com/edugames/model/InfoPanel.java
@@ -29,10 +29,6 @@ public class InfoPanel {
     private String gameModeImage;
     private GameView gameView;
     private boolean startIsVisible = true;
-    private int numberOfCarriers = 1;
-    private int numberOfBattleships = 2;
-    private int numberOfCruisers = 3;
-    private int numberOfSubmarines = 4;
 
     // Constructor
     public InfoPanel(GameView gameView) {
@@ -57,13 +53,6 @@ public class InfoPanel {
         }
     }
 
-    public void updateShipCounts() {
-        numberOfCarriers = gameView.getGameController().getNumberOfShipsOfSize(5);
-        numberOfBattleships = gameView.getGameController().getNumberOfShipsOfSize(4);
-        numberOfCruisers = gameView.getGameController().getNumberOfShipsOfSize(3);
-        numberOfSubmarines = gameView.getGameController().getNumberOfShipsOfSize(2);
-    }
-
     /*
      * Method drawInfoPanel
      * A method that updates the InfoPanel that displays information about remaining ships and what mode user is playing in.
@@ -74,31 +63,6 @@ public class InfoPanel {
     public GridPane drawInfoPanel() {
         // Create new GridPane to contain the entire info panel
         GridPane infoPanelPane = new GridPane();
-        // Create new Images and ImageViews for the ships and assign the Image and assign the variables values
-        Image airCraftCarrier = new Image("airCraftCarrier.png");
-        ImageView airCraftCarrierImageView = new ImageView(airCraftCarrier);
-        Image battleShip = new Image("battleship.png");
-        ImageView battleShipImageView = new ImageView(battleShip);
-        Image cruiser = new Image("cruiser.png");
-        ImageView cruiserImageView = new ImageView(cruiser);
-        Image submarine = new Image("submarine.png");
-        ImageView submarineImageView = new ImageView(submarine);
-        // --TODO-- Replace String numbers with a call to AIPlayers method to return number of ships of a given type
-        // Create new Labels and assign values to the variables, make a toString to print out the Integer
-        Label airCraftCarrierCountLabel = new Label("  x " + numberOfCarriers);
-        Label battleshipCountLabel = new Label("  x " + numberOfBattleships);
-        Label cruiserCountLabel = new Label("  x " + numberOfCruisers);
-        Label submarineCountLabel = new Label("  x " + numberOfSubmarines);
-        // Set font style and size of the labels
-        airCraftCarrierCountLabel.setFont(new Font("Impact", 20));
-        battleshipCountLabel.setFont(new Font("Impact", 20));
-        cruiserCountLabel.setFont(new Font("Impact", 20));
-        submarineCountLabel.setFont(new Font("Impact", 20));
-        // Set color and opacity of the labels
-        airCraftCarrierCountLabel.setTextFill(Color.web("0b00e3", 1));
-        battleshipCountLabel.setTextFill(Color.web("0b00e3", 1));
-        cruiserCountLabel.setTextFill(Color.web("0b00e3", 1));
-        submarineCountLabel.setTextFill(Color.web("0b00e3", 1));
         // Create new Image "playing as"
         Image playingAs = new Image("playingAs.png");
         // Create new ImageView where playingAs is presented
@@ -182,14 +146,6 @@ public class InfoPanel {
         infoPanelPane.getColumnConstraints().add(column5);
         infoPanelPane.getColumnConstraints().add(column6);
         // Add the nodes to the GridPane and set their position
-        infoPanelPane.add(airCraftCarrierImageView, 0, 0);
-        infoPanelPane.add(airCraftCarrierCountLabel, 1, 0);
-        infoPanelPane.add(battleShipImageView,0,1);
-        infoPanelPane.add(battleshipCountLabel, 1,1);
-        infoPanelPane.add(cruiserImageView, 2, 0);
-        infoPanelPane.add(cruiserCountLabel, 3, 0);
-        infoPanelPane.add(submarineImageView, 2, 1);
-        infoPanelPane.add(submarineCountLabel, 3, 1);
         infoPanelPane.add(emptySpace1, 4, 0);
         infoPanelPane.add(gameSpeedDelayLabel, 5,0);
         infoPanelPane.add(gameSpeedDelaySlider,5,1);
